31e03728318f2bef0d8caad76f81a3219bd4606c
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-19 15:39:50 -0300

    test: ignora serviços de voz no contexto de testes

--- src/main/java/com/kelwin/assistente_financeiro/application/service/PiperService.java
+++ src/main/java/com/kelwin/assistente_financeiro/application/service/PiperService.java
@@ -5,8 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("!test")
 @Service
 public class PiperService {
 
