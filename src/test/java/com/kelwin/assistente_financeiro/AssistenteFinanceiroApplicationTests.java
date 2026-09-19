4d0a4dd4eeb5b3aff553b83ff0444568034144c5
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-09-19 15:45:10 -0300

    test: configura perfil de testes no contexto

--- src/test/java/com/kelwin/assistente_financeiro/AssistenteFinanceiroApplicationTests.java
+++ src/test/java/com/kelwin/assistente_financeiro/AssistenteFinanceiroApplicationTests.java
@@ -2,8 +2,10 @@ package com.kelwin.assistente_financeiro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AssistenteFinanceiroApplicationTests {
 
 	@Test
